6f94fdf3edc9304199d84c570691d7704fb6f598
Stefano <stefanop21@outlook.es>
2024-06-22 17:07:42 -0500

    update: crud - read

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,14 +1,41 @@
 import { Request, Response } from 'express';
 
+const todos = [
+  { id: 1, text: 'buy milk', createdAt: new Date() },
+  { id: 2, text: 'buy coffee', createdAt: null },
+  { id: 3, text: 'buy bread', createdAt: new Date() },
+];
+
 export class TodoController {
   //* DI
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
-    res.json([
-      { id: 1, text: 'buy milk', createdAt: new Date() },
-      { id: 2, text: 'buy coffee', createdAt: null },
-      { id: 3, text: 'buy bread', createdAt: new Date() },
-    ]);
+    return res.json(todos);
+  };
+
+  public getTodoById = (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    if (isNaN(id)) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Id argument is not a number',
+      });
+    }
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (!todo) {
+      return res.status(404).json({
+        ok: false,
+        msg: `Todo with id ${id} not found`,
+      });
+    }
+
+    return res.status(200).json({
+      ok: true,
+      todo,
+    });
   };
 }
