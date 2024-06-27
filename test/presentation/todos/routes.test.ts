2a579b245cae7004a11b23e6f46b4c82268f06b5
Stefano <stefanop21@outlook.es>
2024-06-27 17:15:54 -0500

    test: routes - get(api/todos)

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -1,16 +1,27 @@
 import request from 'supertest';
 import { testServer } from '../test-server';
+import { prisma } from '../../../src/data/postgresql';
 
 beforeAll(async () => {
   await testServer.start();
 });
 
-afterAll(() => {
+afterAll(async () => {
   testServer.close();
 });
 
 describe('Testing routes.ts', () => {
-  it('should return todos endpoint', async () => {
-    await request(testServer.app).get('/api/todos').expect(200);
+  const todos = [{ text: 'Todo test 1' }, { text: 'Todo test 2' }];
+
+  it('should return todos api/todos', async () => {
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({ data: todos });
+
+    const { body } = await request(testServer.app)
+      .get('/api/todos')
+      .expect(200);
+
+    expect(body).toBeInstanceOf(Object);
+    expect(body.todos.length).toBe(2);
   });
 });
